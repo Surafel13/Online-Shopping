22b6c78407b950f00dabcc1b22eccfdf2a420c41
Surafel Tesfaye <tesfayesurafel18@gmail.com>
2026-01-29 19:29:50 -1100

    Get user profile controller implemented

--- Backend/controller/UserController.js
+++ Backend/controller/UserController.js
@@ -47,3 +47,19 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ message: "Server error", error });
   } 
 };
+
+
+export const getUserProfile = async (req, res) => {
+    try {
+        const userId = req.body.userId;
+
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
+};
