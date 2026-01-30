f99deca3cb1ce8e3594777484b83668162ad8841
Surafel Tesfaye <tesfayesurafel18@gmail.com>
2026-01-29 19:29:01 -1100

    Users login controller implemented

--- Backend/controller/UserController.js
+++ Backend/controller/UserController.js
@@ -24,3 +24,26 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: "Server error", error });
   } 
 };
+
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }  
+     
+    res.status(200).json({ message: "Login successful" ,
+        user: { id: user._id, username: user.username, email: user.email, role: user.role}
+    });
+
+    } catch (error) {   
+    res.status(500).json({ message: "Server error", error });
+  } 
+};
