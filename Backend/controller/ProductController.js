cbff6c292ea8395215af232a9033a625eb285124
Surafel Tesfaye <tesfayesurafel18@gmail.com>
2026-01-30 22:43:55 -1100

    Product get and update controller functions are added

--- Backend/controller/ProductController.js
+++ Backend/controller/ProductController.js
@@ -16,3 +16,43 @@ export const createProduct = async (req, res) => {
     res.status(500).json({ message: "Server error", error });
   }            
 };
+
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+export const getProductById = async (req, res) => {
+    try {   
+        const productId = req.body.productId;
+        const product = await Product.findById(productId);
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }   
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }   
+
+};
+
+export const updateProduct = async (req, res) => {
+    try {
+        const productId = req.params.productId;
+        const updates = req.body;   
+        const updatedProduct = await Product.findByIdAndUpdate(productId, updates, { new: true });
+
+        if (!updatedProduct) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json({ message: "Product updated successfully", product: updatedProduct });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }   
+};
