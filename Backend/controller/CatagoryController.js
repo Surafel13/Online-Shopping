15039d170d3da0857f757d9ed9ea83a148537daa
Surafel Tesfaye <tesfayesurafel18@gmail.com>
2026-01-30 22:40:30 -1100

    get catagory functionalities added

--- Backend/controller/CatagoryController.js
+++ Backend/controller/CatagoryController.js
@@ -11,3 +11,29 @@ export const createCatagory = async (req, res) => {
         res.status(500).json({ message: "Server error", error });
     }            
 }
+
+
+export const getAllCatagories = async (req, res) => {
+    try {
+        const catagories = await Catagory.find();   
+        res.status(200).json(catagories);
+    }
+    catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }   
+};
+
+export const getCatagoryById = async (req, res) => {
+    try {   
+        const catagoryId = req.body.catagoryId;
+        const catagory = await Catagory.findById(catagoryId);   
+        if (!catagory) {
+            return res.status(404).json({ message: "Catagory not found" });
+        }
+        res.status(200).json(catagory);
+    }
+    catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }   
+
+};
